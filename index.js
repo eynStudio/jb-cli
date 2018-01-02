981674dd558b60713d786cd5065876419d5938de
pswang <pengshengwang@gmail.com>
2018-01-02 16:17:03 +0800

    add more

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
 'use strict'
 
 const program = require('commander')
+const exists = require('fs').existsSync
 const chalk = require('chalk')
+const path = require('path')
+const home = require('user-home')
+const vue = require('./lib/vue')
+const vueNode = require('./lib/vue-node')
 
 program
     .usage('<template-name> [project-name]')
 
 
 program.on('--help', () => {
-    console.log()
     console.log('  Examples:')
     console.log()
     console.log(chalk.gray('    # help info'))
@@ -21,3 +25,44 @@ function help() {
     if (program.args.length < 1) return program.help()
 }
 help()
+
+let template = program.args[0]
+const hasSlash = template.indexOf('/') > -1
+const rawName = program.args[1]
+const inPlace = !rawName || rawName === '.'
+const name = inPlace ? path.relative('../', process.cwd()) : rawName
+const to = path.resolve(rawName || '.')
+const clone = program.clone || false
+
+const tmp = path.join(home, '.vue-templates', template.replace(/\//g, '-'))
+if (program.offline) {
+    console.log(`> Use cached template at ${chalk.yellow(tildify(tmp))}`)
+    template = tmp
+}
+
+
+console.log()
+process.on('exit', () => {
+    console.log()
+})
+
+if (exists(to)) {
+    inquirer.prompt([{
+        type: 'confirm',
+        message: inPlace
+            ? 'Generate project in current directory?'
+            : 'Target directory exists. Continue?',
+        name: 'ok'
+    }]).then(answers => {
+        if (answers.ok) {
+            run()
+        }
+    }).catch(logger.fatal)
+} else {
+    run()
+}
+
+function run() {
+    vue.start()
+    vueNode.start()
+}
