eccbf9e8935422015a75dac85b8360c0fe433bd1
pswang <pengshengwang@gmail.com>
2018-01-02 15:28:37 +0800

    add more

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 'use strict'
 
-console.log('Hello jb-cli!')
+const program = require('commander')
+const chalk = require('chalk')
+
+program
+    .usage('<template-name> [project-name]')
+
+
+program.on('--help', () => {
+    console.log()
+    console.log('  Examples:')
+    console.log()
+    console.log(chalk.gray('    # help info'))
+    console.log()
+})
+
+function help() {
+    program.parse(process.argv)
+    if (program.args.length < 1) return program.help()
+}
+help()
